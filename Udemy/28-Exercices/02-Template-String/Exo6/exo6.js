de86e435a72b4ce3924755dd9c6b362d1a1a1239
danivent1965 <dani.vent@orange.fr>
2023-04-19 11:46:30 +0200

    add exo6 Template-String

---
+++ Udemy/28-Exercices/02-Template-String/Exo6/exo6.js
@@ -0,0 +1,22 @@
+//
+const divResultat = document.querySelector("#resultat");
+let tab = [12, 5, 28, 100];
+// padStart
+for (let i = 0; i < tab.length; i++) {
+    const element = tab[i].toString().padStart(3, 0);
+    console.log(element);
+
+    const baliseH3 = document.createElement("h3");
+    divResultat.appendChild(baliseH3);
+    baliseH3.innerHTML = element;
+}
+
+// padEnd
+for (let i = 0; i < tab.length; i++) {
+    const element = tab[i].toString().padEnd(3, 0);
+    console.log(element);
+
+    const baliseH3 = document.createElement("h3");
+    divResultat.appendChild(baliseH3);
+    baliseH3.innerHTML = `<h3 style="color: red"> ${element}`;
+}
