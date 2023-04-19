09b138dd850e1319f08904ffa8180151b68c9c30
danivent1965 <dani.vent@orange.fr>
2023-04-19 09:04:16 +0200

    upgrade  exo3

--- Udemy/28-Exercices/Let-Const/Exo3/exo3.js
+++ Udemy/28-Exercices/Let-Const/Exo3/exo3.js
@@ -2,6 +2,7 @@ const nb = document.querySelector("#rayon");
 const divResultat = document.querySelector("#resultat");
 const perimetreCheckbox = document.querySelector("#perimetre");
 const valid = document.querySelector(".btn");
+const razBtn = document.querySelector(".btn-raz");
 
 valid.addEventListener("click", () => {
     let resultat = 0;
@@ -10,11 +11,21 @@ valid.addEventListener("click", () => {
     } else {
         resultat = aire(nb.value);
     }
-    divResultat.innerHTML = resultat;
+    const baliseH3 = document.createElement("h3");
+    divResultat.appendChild(baliseH3);
+    baliseH3.innerHTML = resultat;
+
     function perimetre(x) {
-        return "Périmètre : " + Math.PI * x * 2;
+        let perimetre = Math.PI * x * 2;
+        return "Périmètre : " + perimetre.toFixed(2);
     }
     function aire(x) {
-        return "Aire : " + Math.PI * Math.pow(x, 2);
+        let aire = Math.PI * Math.pow(x, 2);
+        return "Aire : " + aire.toFixed(2);
     }
+});
+
+razBtn.addEventListener("click", () => {
+    nb.value = [];
+    divResultat.innerHTML = "";
 });
