c4fa0b0db58f0e3927f867fc3ac7eb2ac6290a5e
danivent42 <danvent42@gmail.com>
2023-12-22 10:35:04 +0100

    ajout leçon ajax

---
+++ EcoleDuWeb/js-asynchrone/ajax/script.js
@@ -0,0 +1,36 @@
+/**
+ * AJAX Asynchronous JavaScript And XML
+ * XMLHttpRequest
+ * https://api.thecatapi.com/v1/images/search
+ */
+// Construction de la requete
+function getCatImg(url, callback) {
+    const xhr = new XMLHttpRequest();
+    console.log(xhr);
+    // Initialisation d'un appel
+    xhr.open("GET", url, true);
+    // Type de reponse
+    xhr.responseType = "json";
+    // Fonction callback
+    xhr.addEventListener("load", handleLoad);
+    function handleLoad() {
+        callback(xhr.response);
+    }
+    // Envoi de la requete
+    xhr.send();
+}
+
+// Envoi de plusieurs images
+const btn = document.querySelector("button");
+btn.addEventListener("click", e => {
+    e.preventDefault();
+    // Requete
+    getCatImg("https://api.thecatapi.com/v1/images/search", data => {
+        console.log(data);
+        const contentImg = document.querySelector(".contentImg");
+        const img = document.createElement("img");
+        img.src = data[0].url;
+        // contentImg.appendChild(img);
+        contentImg.innerHTML = '<img src="' + img.src + '">';
+    });
+});
