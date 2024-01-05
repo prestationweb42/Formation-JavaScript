0c1d9341fca42a8251d11e948bc3370dae06c037
danivent42 <danvent42@gmail.com>
2024-01-05 17:13:17 +0100

    update all projet Avant / Apres

---
+++ EcoleDuWeb/AvantApres/1.Starter/script.js
@@ -0,0 +1,11 @@
+const range = document.querySelector(".disclosure__range");
+const imgContainer2 = document.querySelector(".disclosure__img-container--2");
+const dragLine = document.querySelector(".disclosure__line");
+
+range.addEventListener("input", handleRange);
+
+function handleRange() {
+    // console.log(range.value);
+    imgContainer2.style.inlineSize = range.value + "%";
+    dragLine.style.left = range.value + "%";
+}
