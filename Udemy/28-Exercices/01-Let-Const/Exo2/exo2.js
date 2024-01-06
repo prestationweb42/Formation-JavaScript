8bb3d196002f6e442fc2ff65f5d2da524c48ef59
prestationweb42 <dani.vent@orange.fr>
2024-01-06 11:25:37 +0100

    update css exo2

--- Udemy/28-Exercices/01-Let-Const/Exo2/exo2.js
+++ Udemy/28-Exercices/01-Let-Const/Exo2/exo2.js
@@ -5,14 +5,6 @@ for (let i = 0; i <= a; i++) {
 }
 console.log("la somme de " + 5 + " est : " + sum);
 
-// Factorielle
-// let b = 5;
-// let facto = 1;
-// for (let i = 1; i <= b; i++) {
-//     facto *= i;
-// }
-// console.log("la factorielle de " + 5 + " est : " + facto);
-//
 const numFacto = document.querySelector("#number");
 const valid = document.querySelector(".btn");
 const divResult = document.querySelector(".resultat");
@@ -25,5 +17,6 @@ valid.addEventListener("click", () => {
         facto *= i;
     }
     console.log("la factorielle de " + b + " est : " + facto);
-    divResult.innerHTML = "La factorielle de " + b + " est " + facto;
+    // divResult.innerHTML = "La factorielle de " + b + " est " + facto;
+    divResult.textContent = "La factorielle de " + b + " est " + facto;
 });
