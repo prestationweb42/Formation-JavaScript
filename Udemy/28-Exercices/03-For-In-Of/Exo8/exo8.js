4b858091337b75151b5e979b27dccf874f818400
prestationweb42 <dani.vent@orange.fr>
2024-01-06 21:43:25 +0100

    update CSS exo8 for in of

--- Udemy/28-Exercices/03-For-In-Of/Exo8/exo8.js
+++ Udemy/28-Exercices/03-For-In-Of/Exo8/exo8.js
@@ -1,3 +1,22 @@
 /**
  * Exo 8
  */
+let noms = [];
+const inputNom = document.querySelector("#nom");
+const btn = document.querySelector(".btn");
+const divResultat = document.querySelector(".resultat");
+
+btn.addEventListener("click", function () {
+    noms.push(inputNom.value);
+    afficherNoms();
+    inputNom.value = null;
+});
+
+function afficherNoms() {
+    let list = "<ul>";
+    for (const nom of noms) {
+        list += `<li>${nom}</li>`;
+    }
+    list += "</ul>";
+    divResultat.innerHTML = list;
+}
