52f420d206b12cb5963afd3c4e3f05095816c34c
prestationweb42 <dani.vent@orange.fr>
2024-01-11 08:46:22 +0100

    update lesson 10 Tableaux

--- Udemy/28-Exercices/05-Fonctions-Tableaux/Exo10/exo10.js
+++ Udemy/28-Exercices/05-Fonctions-Tableaux/Exo10/exo10.js
@@ -1,11 +1,24 @@
 /**
  * 05 - Les fonctions sur les tableaux
+ * Exo 10 - Utiliser la boucle for of et entries (destructuring)
+ * N'afficher que les résultats pairs -> even
+ * Et impair -> odd
  */
+const even = document.querySelector(".even");
+const odd = document.querySelector(".odd");
+
 const notes = [5, 10, 15, 20];
 console.log(notes);
-// push -> Ajoute un element a la fin du tableau
-notes.push(23);
-console.log(notes);
-// pop -> supprime un element a la fin du tableau
-notes.pop(23);
-console.log(notes);
+
+for (let [indice, note] of notes.entries()) {
+    if (note % 2 === 0) {
+        console.log(`Note = ${indice}, ${note}`);
+        even.innerHTML += "<h3>" + `Note = ${indice}, ${note}` + "</h3>";
+    } else {
+        console.log('--------');
+        console.log(`Note = ${indice}, ${note}`);
+        const baliseH3 = document.createElement("h3");
+        baliseH3.innerHTML = `Indice = ${indice}, Note = ${note}`;
+        odd.appendChild(baliseH3);
+    }
+}
